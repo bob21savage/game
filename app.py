e68fa400f53b36d4217e2dfd21fdb6bb44b39b1e
bob21savage <58413393+bob21savage@users.noreply.github.com>
2025-01-26 16:39:03 +0000

    updated the ports

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, RadioField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo
 import json
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '666'
@@ -861,5 +862,6 @@ def gameplay():
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if not set
+    app.run(host='0.0.0.0', port=port)
